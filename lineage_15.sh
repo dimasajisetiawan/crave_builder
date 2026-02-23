dbbfcbd942bc5ed38af26ee7defd59651b8582ed
Dimas Aji Setiawan <90007420+dimasajisetiawan@users.noreply.github.com>
2026-02-23 20:36:22 +0700

    Add files via upload

---
+++ lineage_15.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# WARNING: This will remove all local changes!
+rm -rf .repo/local_manifests
+rm -rf kernel/sony
+rm -rf device/sony
+rm -rf hardware/sony
+rm -rf vendor/sony
+rm -rf vendor/lineage-priv
+
+# Initialize repo
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
+
+# Sync the repositories
+/opt/crave/resync.sh
+/opt/crave/resync.sh
+repo sync
+
+# Clone device tree
+git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b stock kernel/sony/sdm845
+git clone https://github.com/Sorayukii/android_device_sony_akatsuki -b 15 device/sony/akatsuki
+git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 15 device/sony/tama-common
+git clone https://github.com/Sorayukii/android_hardware_sony_SonyOpenTelephony -b 15 hardware/sony/SonyOpenTelephony
+git clone https://github.com/Sorayukii/proprietary_vendor_sony_akatsuki -b 15 vendor/sony/akatsuki
+git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 15 vendor/sony/tama-common
+git clone https://github.com/Sorayukii/priv-keys -b master vendor/lineage-priv
+
+# Export
+export BUILD_USERNAME=makeroot911
+export BUILD_HOSTNAME=crave
+
+# Set up build environment
+source build/envsetup.sh
+
+# Build rom
+brunch akatsuki
+
+# Upload rom
+curl uploader.sh -T out/target/product/akatsuki/lineage*.zip
