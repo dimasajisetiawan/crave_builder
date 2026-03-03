f7190cd7fcd2742184050f53c1be36930bc8a7e6
Dimas Aji Setiawan <90007420+dimasajisetiawan@users.noreply.github.com>
2026-03-03 13:09:08 +0700

    Create los_a16_santoni.sh

---
+++ los_a16_santoni.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# WARNING: This will remove all local changes!
+rm -rf .repo/local_manifests
+rm -rf kernel/sony
+rm -rf device/sony
+rm -rf hardware/sony
+rm -rf vendor/sony
+rm -rf vendor/lineage-priv
+
+# Initialize repo
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+
+# Sync the repositories
+/opt/crave/resync.sh
+/opt/crave/resync.sh
+repo sync
+
+# Clone device tree
+git clone https://github.com/dimasajisetiawan/local_manifests .repo/local_manifests
+
+
+# Export
+export BUILD_USERNAME=makeroot911
+export BUILD_HOSTNAME=crave
+
+# Set up build environment
+source build/envsetup.sh
+
+# Build rom
+brunch Mi8937
+
+# Upload rom
+curl uploader.sh -T out/target/product/Mi8937/lineage*.zip
