84453a9e6460530d6bb21f743bd92c8ae94f529e
Dimas Aji Setiawan <90007420+dimasajisetiawan@users.noreply.github.com>
2026-03-04 22:04:13 +0700

    Create twrp_pbrp_akatsuki.sh

---
+++ twrp_pbrp_akatsuki.sh
@@ -0,0 +1,7 @@
+repo init -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-12.1
+/opt/crave/resync.sh
+repo sync
+git clone https://github.com/j4nn/android_device_sony_tama.git -b android-12.1 device/sony/tama
+source build/envsetup.sh
+lunch pb_akatsuki-eng
+mka pbrp
