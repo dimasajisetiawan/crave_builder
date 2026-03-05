d39ff7233bdf25e33029acd85879894225c3e60f
Dimas Aji Setiawan <90007420+dimasajisetiawan@users.noreply.github.com>
2026-03-05 20:07:17 +0700

    Create shrp_akatsuki.sh

---
+++ shrp_akatsuki.sh
@@ -0,0 +1,8 @@
+repo init -u https://github.com/SHRP/manifest.git -b shrp-12.1
+/opt/crave/resync.sh
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+git clone https://github.com/j4nn/android_device_sony_tama.git -b android-12.1 device/sony/tama
+export ALLOW_MISSING_DEPENDENCIES=true
+source build/envsetup.sh
+lunch twrp_akatsuki-eng
+mka bootimage
