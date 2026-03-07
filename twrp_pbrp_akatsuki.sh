23d4276016732e5f476e905c596713f5c821e288
Dimas Aji Setiawan <90007420+dimasajisetiawan@users.noreply.github.com>
2026-03-07 12:47:57 +0700

    Update twrp_pbrp_akatsuki.sh

--- twrp_pbrp_akatsuki.sh
+++ twrp_pbrp_akatsuki.sh
@@ -1,7 +1,8 @@
 repo init -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-12.1
 /opt/crave/resync.sh
 repo sync
-git clone https://github.com/dimasajisetiawan/android_device_sony_tama.git -b android-12.1 device/sony/tama
+git clone https://github.com/j4nn/android_device_sony_tama -b android-12.1 device/sony/tama
+patch -p1 < device/sony/tama/patches/vold-decryption-workaround-for-sodp-kernel.patch
 source build/envsetup.sh
 lunch twrp_akatsuki-eng
 mka pbrp
