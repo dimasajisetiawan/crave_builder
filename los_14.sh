b477bff8cfaf0bc4db574acc496ca5cf2d427b1f
Dimas Aji Setiawan <90007420+dimasajisetiawan@users.noreply.github.com>
2026-03-17 14:41:19 +0700

    Update los_14.sh

--- los_14.sh
+++ los_14.sh
@@ -17,7 +17,13 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 repo sync
 
 # Clone device tree
-git clone https://github.com/dimasajisetiawan/local_manifests -b los_14 .repo/local_manifests
+git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b stock kernel/sony/sdm845
+git clone https://github.com/sorayuki01/android_device_sony_akatsuki -b 14 device/sony/akatsuki
+git clone https://github.com/sorayuki01/android_device_sony_tama-common -b 14 device/sony/tama-common
+git clone https://github.com/sorayuki01/android_hardware_sony_SonyOpenTelephony -b lineage-21 hardware/sony/SonyOpenTelephony
+git clone https://github.com/sorayuki01/proprietary_vendor_sony_akatsuki -b lineage-21 vendor/sony/akatsuki
+git clone https://github.com/sorayuki01/proprietary_vendor_sony_tama-common -b 14 vendor/sony/tama-common
+git clone https://github.com/sorayuki01/priv-keys -b master vendor/lineage-priv
 
 
 # Export
@@ -28,7 +34,7 @@ export BUILD_HOSTNAME=crave
 source build/envsetup.sh
 
 # Build rom
-brunch akatsuki
-
+breakfast akatsuki userdebug;
+mka bacon
 # Upload rom
 curl uploader.sh -T out/target/product/akatsuki/lineage*.zip
