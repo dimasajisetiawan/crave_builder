c00b5dbd91d9f0afcc40ec165760c199c5ea4a9a
Dimas Aji Setiawan <90007420+dimasajisetiawan@users.noreply.github.com>
2026-03-22 19:38:49 +0700

    Update matrix-14.sh

--- matrix-14.sh
+++ matrix-14.sh
@@ -8,12 +8,19 @@ rm -rf prebuilts/clang/host/linux-x86
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 
 # Clone device tree manifest
-git clone https://github.com/dimasajisetiawan/local_manifests -b los_a14 .repo/local_manifests
+git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b stock kernel/sony/sdm845
+git clone https://github.com/sorayuki01/android_device_sony_akatsuki -b 14 device/sony/akatsuki
+git clone https://github.com/sorayuki01/android_device_sony_tama-common -b 14 device/sony/tama-common
+git clone https://github.com/sorayuki01/android_hardware_sony_SonyOpenTelephony -b lineage-21 hardware/sony/SonyOpenTelephony
+git clone https://github.com/sorayuki01/proprietary_vendor_sony_akatsuki -b lineage-21 vendor/sony/akatsuki
+git clone https://github.com/sorayuki01/proprietary_vendor_sony_tama-common -b 14 vendor/sony/tama-common
+git clone https://github.com/sorayuki01/priv-keys -b master vendor/lineage-priv
+
 
 # Sync the repositories
 /opt/crave/resync.sh
 /opt/crave/resync.sh
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync
 
 # Export
 export BUILD_USERNAME=makeroot911
